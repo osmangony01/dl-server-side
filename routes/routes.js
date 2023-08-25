9d68cb5c8d6c206a9cc9f1a980ff465ee04a081a
osmangony01 <mdosmangoni376@gmail.com>
2023-08-26 01:15:14 +0600

    create controller and add router to get specific category data

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,8 @@
 
 const route = require('express').Router();
 const { getAllTaskData, getAllCategory } = require('../controllers/categoryController');
-const { getAllReserveData } = require('../controllers/reserveData');
+const { getAllReserveData } = require('../controllers/reserveDataController');
+const { getSpecificCategoryData } = require('../controllers/specificCategoryDataController');
 
 
 route.get("/api/data", getAllTaskData);
@@ -12,6 +13,9 @@ route.get('/api/category', getAllCategory);
 
 // get all category data
 route.get("/api/getAllCategoryData", getAllReserveData);
+
+// get all data of specific category
+route.get("/api/specificCategoryData", getSpecificCategoryData);
 
 
 module.exports = route;
