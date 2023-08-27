93cfb77fd34b29f5a20d99559034a50a83830dd6
osmangony01 <mdosmangoni376@gmail.com>
2023-08-27 12:43:07 +0600

    add search functionlity in searchController

--- controllers/searchResultController.js
+++ controllers/searchResultController.js
@@ -3,23 +3,40 @@ const ReserveData = require('../models/reserveModels');
 
 const getAllSearchResultData = async (req, res) => {
     const queryResult = req.query;
+    // const queryResult = {
+    //     location: 'chittagong, Bangladesh',
+    //     checkIn: '2023-08-10',
+    //     checkOut: '2023-08-15',
+    //     guestCapacity: '25'
+    // };
+    const checkInDate = new Date(queryResult.checkIn);
+    const checkOutDate = new Date(queryResult.checkOut);
+
     console.log(queryResult);
     //const searchQuery = {};
 
     const searchQuery = {
-        location: { $regex: queryResult.location, $options: 'i' }, // Searching location with "chittagong"
-        $and: [
-          { checkIn: { $lt: queryResult.checkIn } }, // Check-in date in the database is before to the given check-in date
-          { checkOut: { $gt: queryResult.checkIn } }, // Check-out date in the database is after or equal to the  given check-in date
-          { checkIn: { $lt: queryResult.checkOut } }, // Check-in date in the database is before or equal to the  given check-out date
-          { checkOut: { $gt: queryResult.checkOut } }, // Check-out date in the database is after or equal to the given check-out date
+        location: { $regex: queryResult.location, $options: 'i' },
+        $or: [
+            /* my check-in and check-out date is either less  than the database check-in 
+               or grater than the database check-out */
+            {
+                $and: [
+                    { checkIn: { $lt: checkInDate } },
+                    { checkOut: { $lt: checkInDate } }
+                ]
+            },
+            {
+                $and: [
+                    { checkIn: { $gt: checkOutDate } },
+                    { checkOut: { $gt: checkOutDate } }
+                ]
+            }
         ],
-        guestCapacity: { $lte: queryResult.guestCapacity } // Guest capacity is at least 6
-      };
+        guestCapacity: { $gte: parseInt(queryResult.guestCapacity) }
+    };
 
-    
-
-    
+    console.log(searchQuery)
     try {
         const data = await ReserveData.find(searchQuery);
         console.log(data);
